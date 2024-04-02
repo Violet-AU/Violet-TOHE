2030f7060e44cb64d41607695e45ba07d6f9267b
Theta <82065263+ThetaHalo@users.noreply.github.com>
2024-04-02 15:57:17 +0000

    Remove Source

--- Roles/Impostor/Bard.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TOHE.Roles.Impostor;
-
-class Bard
-{
-    public static void OnExileWrapUp(PlayerControl Bard, GameData.PlayerInfo exiled)
-    {
-        if (exiled != null) Main.AllPlayerKillCooldown[Bard.PlayerId] /= 2;
-    }
-}
